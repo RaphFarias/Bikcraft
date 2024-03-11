f921f1ffdf1103deeab4d33ca49c8ad23f9c2118
Raphael Farias <raphaelfarias.dev@gmail.com>
2024-03-11 18:11:51 -0300

    otimizacoes

--- js/script.js
+++ js/script.js
@@ -44,20 +44,14 @@ const galeriaContainer = document.querySelector('.bicicleta-imagens');
 
 function trocarImagem(event) {
   const img = event.currentTarget;
-  const media = matchMedia(("min-width: 1000px")).matches;
+  const media = matchMedia('(min-width: 1000px)').matches;
   if(media) {
     galeriaContainer.prepend(img);
   }
 }
 
 function eventosGaleria(img) {
-  img.addEventListener('click', trocarImgem);
+  img.addEventListener('click', trocarImagem);
 }
-galeria.forEach();
+galeria.forEach(eventosGaleria, 'olá');
 
-
-// Animação 
-
-if (window.SimpleAnime) {
-  new SimpleAnime()
-}
